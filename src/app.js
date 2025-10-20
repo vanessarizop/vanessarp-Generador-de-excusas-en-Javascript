587cf6f106a1afb195c95b4dbba5e6b9e02d0d62
vanessarizop <vanessa.rizop@gmail.com>
2025-10-20 22:49:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import "bootstrap";
 import "./style.css";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function  excuseGenerator(array){
+  let nroRandom = Math.floor(Math.random() * array.length);
+ return array[nroRandom];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+   let randomExcuse = excuseGenerator(who) +" "+ excuseGenerator(action) +" "+excuseGenerator(what) +" "+ excuseGenerator(when);
+  document.getElementById("excuse").innerText =randomExcuse;
+      };
